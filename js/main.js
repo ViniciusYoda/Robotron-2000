1c9c70892295225f8ff07fbef4cb31ee4b2bf390
Vinícius Yoda <vyoda4604@gmail.com>
2022-12-27 14:59:30 -0300

    fo

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
-const robotron = document.querySelector("#robotron")
+const subtrair = document.querySelector("#subtrair")
+const somar = document.querySelector("#somar");
+const braco = document.querySelector("#braco")7
 
-robotron.addEventListener("click", (evento) => {
-   console.log(evento)
+const controle = document.querySelectorAll(".controle-ajuste")
+
+controle.forEach( (elemento) => {
+   elemento.addEventListener("click", (evento) => {
+      manipulaDados(evento.target.textContent)
+   })
 })
 
-function dizOi(nome) {
-   console.log('Oi ' + nome)
-   console.log('Bem-vindo ao Robotron 2000')
+function manipulaDados(operacao) {
+   if(operacao === "subtrair") {
+      braco.value = parseInt(braco.value) + 1
+   } else {
+      braco.value = parseInt(braco.value) - 1
+   }
 }
-
-dizOi("Vinícius")
