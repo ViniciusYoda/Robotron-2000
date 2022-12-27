c29ed819cd2e3cb8db389a5e00fab19d549dbed6
Vinícius Yoda <vyoda4604@gmail.com>
2022-12-27 13:03:44 -0300

    segundo passo

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const robotron = document.querySelector("#robotron")
+
+robotron.addEventListener("click", (evento) => {
+   console.log(evento)
+})
+
+function dizOi(nome) {
+   console.log('Oi ' + nome)
+   console.log('Bem-vindo ao Robotron 2000')
+}
+
+dizOi("Vinícius")
